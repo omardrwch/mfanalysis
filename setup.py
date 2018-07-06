36c00e0d4ee346bb6dddc5776a86c8c2ebd4501a
Omar D. Domingues <omar.drwch@gmail.com>
2018-07-06 13:50:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name='mfanalysis',
       version='0.12dev',
       description='Implements wavelet based multifractal analysis of 1d signals.',
-      url='https://gitlab.inria.fr/odarwich/omar_darwiche_domingues',
+      url='https://github.com/omardrwch/mfanalysis',
       author='Omar Darwiche Domingues',
       author_email='',
-      license='',
+      license='MIT',
       packages=['mfanalysis'],
       install_requires=[
           'pywavelets', 'scipy', 'numpy', 'matplotlib',
